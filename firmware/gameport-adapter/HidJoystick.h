cc3946dcf757a8e1b892ef0f66566bba6c0ce36b
Bart Janssens <bart@bartjanssens.org>
2022-08-23 21:32:46 +0200

    Working controller

--- firmware/gameport-adapter/HidJoystick.h
+++ firmware/gameport-adapter/HidJoystick.h
@@ -50,7 +50,7 @@ public:
 
 private:
   using BufferType = Buffer<256>;
-  static const uint8_t DEVICE_ID{3};
+  static const uint8_t DEVICE_ID{1};
 
   static BufferType createDescription(const Joystick &joystick) {
 
@@ -142,67 +142,40 @@ private:
 
 static BufferType createDescriptionGamePad(const Joystick &joystick) {
 
-    enum class ID : uint8_t {
-      physical = 0x00,
-      application = 0x01,
-      button = 0x09,
-      collection = 0xa1,
-      end_collection = 0xc0,
-      generic_desktop = 0x01,
-      hat_switch = 0x39,
-      input = 0x81,
-      input_const = 0x03,
-      input_data = 0x02,
-      gamepad = 0x05,
-      logical_max = 0x26,
-      logical_min = 0x15,
-      report_count = 0x95,
-      report_id = 0x85,
-      report_size = 0x75,
-      simulation_controls = 0x02,
-      throttle = 0xbb,
-      usage = 0x09,
-      usage_max = 0x29,
-      usage_min = 0x19,
-      usage_page = 0x05,
-      usage_x1 = 0x30,
-      usage_y1 = 0x31,
-      usage_x2 = 0x33,
-      usage_y2 = 0x34,
+    const uint8_t hidReportDescriptor [] = 
+    {
+      0x05, 0x01,          // UsagePage(Generic Desktop[1])
+      0x09, 0x05,          // UsageId(Gamepad[5])
+      0xA1, 0x01,          // Collection(Application)
+      0x85, 0x01,          //     ReportId(1)
+      0x09, 0x01,          //     UsageId(Pointer[1])
+      0xA1, 0x00,          //     Collection(Physical)
+      0x09, 0x30,          //         UsageId(X[48])
+      0x09, 0x31,          //         UsageId(Y[49])
+      0x09, 0x32,          //         UsageId(Z[50])
+      0x09, 0x33,          //         UsageId(Rx[51])
+      0x15, 0x00,          //         LogicalMinimum(0)
+      0x26, 0xFF, 0x03,    //         LogicalMaximum(1,023)
+      0x95, 0x04,          //         ReportCount(4)
+      0x75, 0x0A,          //         ReportSize(10)
+      0x81, 0x02,          //         Input(Data, Variable, Absolute, NoWrap, Linear, PreferredState, NoNullPosition, BitField)
+      0xC0,                //     EndCollection()
+      0x05, 0x09,          //     UsagePage(Button[9])
+      0x19, 0x01,          //     UsageIdMin(Button 1[1])
+      0x29, 0x10,          //     UsageIdMax(Button 16[16])
+      0x25, 0x01,          //     LogicalMaximum(1)
+      0x95, 0x10,          //     ReportCount(16)
+      0x75, 0x01,          //     ReportSize(1)
+      0x81, 0x02,          //     Input(Data, Variable, Absolute, NoWrap, Linear, PreferredState, NoNullPosition, BitField)
+      0xC0,                // EndCollection()
     };
 
-    const auto &desc = joystick.getDescription();
     BufferType buffer;
-    auto filler = BufferFiller(buffer);
-
-    filler.push(ID::usage_page).push(ID::generic_desktop);
-    filler.push(ID::usage).push(ID::gamepad);
-    filler.push(ID::collection).push(ID::application);
-    filler.push(ID::collection).push(ID::physical);
-    filler.push(ID::report_id).push(uint8_t(DEVICE_ID));
-
-    filler.push(ID::usage_page).push(ID::button);
-    filler.push(ID::usage_min).push(uint8_t(1));
-    filler.push(ID::usage_max).push(uint8_t(16));
-    filler.push(ID::logical_min).push(uint8_t(0));
-    filler.push(ID::logical_max).push(uint16_t(1));
-    filler.push(ID::report_size).push(uint8_t(1));
-    filler.push(ID::report_count).push(uint8_t(16));
-
-    filler.push(ID::input).push(ID::input_data);
-    filler.push(ID::usage_page).push(ID::generic_desktop);
-    filler.push(ID::usage).push(ID::usage_x1);
-    filler.push(ID::usage).push(ID::usage_y1);
-    filler.push(ID::usage).push(ID::usage_x2);
-    filler.push(ID::usage).push(ID::usage_y2);
-    filler.push(ID::logical_min).push(uint8_t(0));
-    filler.push(ID::logical_max).push(uint16_t(1023));
-    filler.push(ID::report_size).push(uint8_t(10));
-    filler.push(ID::report_count).push(uint8_t(4));
-    filler.push(ID::input).push(ID::input_data);
-
-    filler.push(ID::end_collection);
-    filler.push(ID::end_collection);
+    for(int i = 0; i != sizeof(hidReportDescriptor); ++i)
+    {
+      buffer.data[i] = hidReportDescriptor[i];
+    }
+    buffer.size = sizeof(hidReportDescriptor);
     return buffer;
   }
 
@@ -253,22 +226,10 @@ static BufferType createDescriptionGamePad(const Joystick &joystick) {
     const auto &state = joystick.getState();
     BufferType buffer;
     auto filler = BufferFiller(buffer);
-    uint16_t buttons = state.buttons;
-    
+
     uint8_t h1 = state.hats[0];
     uint8_t h2 = state.hats[1];
     uint8_t h3 = state.hats[2];
-
-    if(hatx(h2) > 512) bitSet(buttons, 9);
-    if(haty(h2) > 512) bitSet(buttons, 10);
-    if(hatx(h2) < 512) bitSet(buttons, 11);
-    if(haty(h2) < 512) bitSet(buttons, 12);
-
-    if(hatx(h3) > 512) bitSet(buttons, 13);
-    if(haty(h3) > 512) bitSet(buttons, 14);
-    if(hatx(h3) < 512) bitSet(buttons, 15);
-
-    filler.push(buttons, 16);
 
     // axes
     uint16_t x2 = hatx(h1);
@@ -277,6 +238,26 @@ static BufferType createDescriptionGamePad(const Joystick &joystick) {
     filler.push(y2, 10);
     filler.push(state.axes[0], 10);
     filler.push(state.axes[1], 10);
+
+    uint16_t buttons = 0;
+    bitWrite(buttons, 0, bitRead(state.buttons,5));
+    bitWrite(buttons, 1, bitRead(state.buttons,3));
+    bitWrite(buttons, 2, bitRead(state.buttons,6));
+    bitWrite(buttons, 3, bitRead(state.buttons,4));
+    bitWrite(buttons, 4, bitRead(state.buttons,7));
+    bitWrite(buttons, 5, bitRead(state.buttons,8));
+    bitWrite(buttons, 6, bitRead(state.buttons,2));
+    bitWrite(buttons, 7, bitRead(state.buttons,0));
+    if(hatx(h2) > 512) bitSet(buttons, 8);
+    if(hatx(h2) < 512) bitSet(buttons, 9);
+    if(haty(h2) < 512) bitSet(buttons, 10);
+    bitWrite(buttons, 11, bitRead(state.buttons,1));
+    if(haty(h3) > 512) bitSet(buttons, 12);
+    if(haty(h3) < 512) bitSet(buttons, 13);
+    if(hatx(h3) < 512) bitSet(buttons, 14);
+    if(hatx(h3) > 512) bitSet(buttons, 15);
+
+    filler.push(buttons, 16);
 
     filler.allign();
     return buffer;
